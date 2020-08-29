d12b475c8d271ff050c68d4b20bc16477974a641
Jefferson Diefenbach <jefferson.diefenbach@uhk.com.br>
2020-08-29 14:05:36 -0300

    Mudança para variaveis de ambiente

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     'Content-type': 'application/json',
   },
